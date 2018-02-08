1cfc17ac0ad62044a4ffa829456623d61b8e5774
by46 <ycs_ctbu_2010@126.com>
2018-02-08 14:07:35 +0800

    feat: fixbug

--- flask_kits/restful/entity/__init__.py
+++ flask_kits/restful/entity/__init__.py
@@ -7,10 +7,15 @@ from .common import LOC_FORM
 from .common import LOC_HEADERS
 from .common import LOC_JSON
 from .pagination import Pagination
-from .response import Response
+from .parser import compatible_bool
+from .parser import compatible_datetime
+from .parser import compatible_decimal
 from .validator import LetterValidator
 from .validator import MaxLengthValidator
 from .validator import MinLengthValidator
 from .validator import MoreValidator
 from .validator import PrecisionValidator
 from .validator import Validator
+from ..response import Response
+
+assert Response is not None
